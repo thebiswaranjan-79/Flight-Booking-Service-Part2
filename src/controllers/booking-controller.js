1c18156632dc5be1e3251c97e5299f19184c0150
Biswaranjan Mohanty <mbiswaranjan797@gmail.com>
2024-08-08 21:01:04 +0530

    Added Booking Payment

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -13,12 +13,33 @@ async function createBooking(req, res) {
     SuccessResponse.data = response;
     return res.status(StatusCodes.OK).json(SuccessResponse);
   } catch (error) {
-    console.log("controller catching");
     ErrorResponse.error = error;
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(ErrorResponse);
+        return res
+                .status(StatusCodes.INTERNAL_SERVER_ERROR)
+                .json(ErrorResponse);
+    }
+}
+
+async function makePayment(req, res) {
+  try {
+      const response = await BookingService.makePayment({
+          totalCost: req.body.totalCost,
+          userId: req.body.userId,
+          bookingId: req.body.bookingId
+      });
+      SuccessResponse.data = response;
+      return res
+              .status(StatusCodes.OK)
+              .json(SuccessResponse);
+  } catch(error) {
+      ErrorResponse.error = error;
+      return res
+              .status(StatusCodes.INTERNAL_SERVER_ERROR)
+              .json(ErrorResponse);
   }
 }
 
 module.exports = {
   createBooking,
+  makePayment
 };
